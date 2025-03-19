51c7875c1da9fffdf96c28bd6449d404f697fd3d
prakashpawar4u <43570466+prakashpawar4u@users.noreply.github.com>
2025-03-19 18:03:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.31%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        count = 0 
-        while n: 
-            count += n & 1
-            n >>= 1
-        return count 
-        
+        # count = 0 
+        # while n: 
+        #     count += n & 1
+        #     n >>= 1
+        # return count 
+        count = 0
+        while n:
+            n = n & (n - 1)  # Flip the least significant set bit
+            count += 1
+        return count
