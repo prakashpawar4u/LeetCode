42218446df6fb18313407575ea43de332bfd7c47
prakashpawar4u <43570466+prakashpawar4u@users.noreply.github.com>
2025-03-19 18:21:17 +0530

    Time: 4 ms (44.22%) | Memory: 17.2 MB (32.76%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -10,14 +10,25 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: bool
         """
-        def in_order_traversal(node):
-            if not node:
-                return []
-            return in_order_traversal(node.left) + [node.val] + in_order_traversal(node.right)
+        # def in_order_traversal(node):
+        #     if not node:
+        #         return []
+        #     return in_order_traversal(node.left) + [node.val] + in_order_traversal(node.right)
         
-        in_order = in_order_traversal(root)
+        # in_order = in_order_traversal(root)
 
-        for i in range(1, len(in_order)):
-            if in_order[i] <= in_order[i -1]:
+        # for i in range(1, len(in_order)):
+        #     if in_order[i] <= in_order[i -1]:
+        #         return False
+        # return True
+        def validate(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            # Check if the current node's value is within the valid range
+            if node.val <= lower or node.val >= upper:
                 return False
-        return True
+            # Recursively validate the left and right subtrees
+            return (validate(node.left, lower, node.val) and
+                    validate(node.right, node.val, upper))
+
+        return validate(root)
