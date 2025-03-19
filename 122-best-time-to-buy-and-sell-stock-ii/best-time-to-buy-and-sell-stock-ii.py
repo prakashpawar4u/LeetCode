d89bc02eaabfd34e0af5e6455a5686967768fc8b
prakashpawar4u <43570466+prakashpawar4u@users.noreply.github.com>
2025-03-19 15:16:44 +0530

    Time: 4 ms (23.99%) | Memory: 13.1 MB (95.92%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,20 +4,20 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # l,r = 0,1
-        # TotalP = 0
-        # while r < len(prices):
-        #     if prices[l] < prices[r]:
-        #         profit = prices[r] - prices[l]
-        #         TotalP = TotalP + profit
-        #     l += 1
-        #     r += 1
-        # return TotalP
+        l,r = 0,1
+        TotalP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                TotalP = TotalP + profit
+            l += 1
+            r += 1
+        return TotalP
 
-        max_profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i -1]:
-                max_profit += prices[i] - prices[i - 1]
-        return max_profit
+        # max_profit = 0
+        # for i in range(1, len(prices)):
+        #     if prices[i] > prices[i -1]:
+        #         max_profit += prices[i] - prices[i - 1]
+        # return max_profit
         
         
