68bd12f66c8350870317b589926ebecc2af23592
prakashpawar4u <43570466+prakashpawar4u@users.noreply.github.com>
2025-06-24 01:27:40 +0530

    Time: 68 ms (21.86%) | Memory: 18.1 MB (35.59%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome_range(i: int, j: int) -> bool:
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                # Try skipping left or right character
+                return is_palindrome_range(left + 1, right) or is_palindrome_range(left, right - 1)
+            left += 1
+            right -= 1
+
+        return True
+            
