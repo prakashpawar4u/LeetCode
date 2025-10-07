b820804c458420f14ebdc68d43ed3883b8982271
prakashpawar4u <43570466+prakashpawar4u@users.noreply.github.com>
2025-10-08 00:32:12 +0530

    Time: 4 ms (91.24%) | Memory: 23.8 MB (5.36%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,6 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        filter_st = []
-        for c in s:
-            if c.isalnum():
-                filter_st.append(c.lower())
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filter_st = [c.lower() for c in s if c.isalnum()]
+        return filter_st == filter_st[::-1]
 
-        filter_st = "".join(filter_st)
-        return (filter_st == filter_st[::-1])
-
-                
+        
